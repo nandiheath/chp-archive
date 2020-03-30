a3d00f96e86e384cfae3a2623d095ea7345d8636
rowena-sc-lai <55628608+rowena-sc-lai@users.noreply.github.com>
2020-03-30 22:55:45 +0800

    Add Extract CHP file python

---
+++ data/extract_chp_share.py
@@ -0,0 +1,124 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 30 22:53:46 2020
+
+@author: rowena
+"""
+
+import os
+os.system('pip install pandas')
+os.system('pip install requests')
+os.system('pip install pathlib')
+os.system('pip install tabula-py')
+
+
+import sys
+
+old_stdout = sys.stdout
+
+log_file = open("test.log","w")
+sys.stdout = log_file
+
+print("this will be written to message.log")
+
+
+
+#pip install tabula-py
+
+import pandas as pd 
+from pathlib import Path
+import requests
+import tabula
+
+from tabula import read_pdf
+from pandas import DataFrame
+
+import time
+timestr = time.strftime("%Y%m%d")
+
+from datetime import datetime, timedelta
+ytd=datetime.strftime(datetime.now() - timedelta(1), '%Y%m%d')
+
+import smtplib
+gmail_user = 'trinsaur@gmail.com'
+gmail_password = 'gyhrwnenvkcugzwo'
+
+sent_from = gmail_user
+to = ['trinsaur@gmail.com']
+subject = 'CHP File Update'
+
+print("Finish Define Variable")
+
+df_log=[]
+
+url='https://www.chp.gov.hk/files/pdf/local_situation_covid19_en.pdf'
+#output='~/Documents/case'+timestr+'.csv'
+str1='case_en'
+
+def extract_chp(url,output,str1):
+    filename = Path('tmp_chp_'+str1+'.pdf')
+    response = requests.get(url)
+    filename.write_bytes(response.content)
+    
+    df=read_pdf('tmp_chp_'+str1+'.pdf', multiple_tables=True, pages="all", lattice=True)
+    #df=tabula.read_pdf('tmp_chp_'+str1+'.pdf', spreadsheet=True)
+    df_all=df[0].replace('\r\n', ' ')
+    for i in range(2,len(df)+1,1):
+        df_all=df_all.append(df[i-1].replace('\n', ' '))
+    
+    #df_all1=df_all[df_all['Unnamed: 0'].notnull()]
+    df_log=DataFrame([len(df_all)])
+    
+    os.system('chmod 750 '+"~/Documents/df_log_"+str1+".csv")
+    tmp_log = pd.read_csv("~/Documents/df_log_"+str1+".csv",header=0,index_col=False ) 
+    lst_log=tmp_log.loc[0,'0']
+  
+    if df_log.loc[0,0] != lst_log:
+        #df_all1.to_csv(output)
+        df_all.to_excel(r'~/Documents/chf_'+str1+'_'+timestr+'.xlsx')
+        df_all.to_csv(r'~/Documents/chf_'+str1+'_'+timestr+'.csv')
+        df_log.to_csv("~/Documents/df_log_"+str1+".csv")
+        
+        body = 'Hey, Update Sucess on '+url
+
+        email_text = """\
+            From: %s
+            To: %s
+            Subject: %s
+
+            %s
+            """ % (sent_from, ", ".join(to), subject, body)
+        
+        try:
+            server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+            server.ehlo()
+            server.login(gmail_user, gmail_password)
+            server.sendmail(sent_from, to, email_text)
+            server.close()
+            print('Email sent!')
+        except:
+            print('Something went wrong...')
+        
+        
+    else:
+        print ('No Update')
+#extract_chp();
+
+#print("Finish define function")
+#extract_chp('https://www.chp.gov.hk/files/pdf/list_of_buildings_en.pdf','~/Documents/home_confiness_eng'+timestr+".csv",'building_en')
+#extract_chp('https://www.chp.gov.hk/files/pdf/list_of_buildings_tc.pdf','~/Documents/home_confiness_chi'+timestr+".csv",'building_tc')
+
+print("Finish Processing building list")
+extract_chp('https://www.chp.gov.hk/files/pdf/local_situation_covid19_tc.pdf','~/Documents/case'+timestr+'.csv','case_tc')
+#extract_chp('https://www.chp.gov.hk/files/pdf/local_situation_covid19_en.pdf','~/Documents/case'+timestr+'.csv','case_en')
+
+extract_chp('https://www.chp.gov.hk/files/pdf/building_list_chi.pdf','~/Documents/case'+timestr+'.csv','building_tc')
+#extract_chp('https://www.chp.gov.hk/files/pdf/building_list_eng.pdf','~/Documents/case'+timestr+'.csv','building_en')
+
+
+
+#extract_chp('https://www.chp.gov.hk/files/pdf/599c_tc.pdf','~/Documents/confinee'+timestr+'.csv','confinee')
+
+sys.stdout = old_stdout
+log_file.close()
